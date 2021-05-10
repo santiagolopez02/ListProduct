df9ed902c8746d163affbda46a3ccf89808ce3bf
Santiago Lopez <lopezsantiago35699@gmail.com>
2021-05-10 17:26:36 -0300

    Create first component

---
+++ listproduct/src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+            <div className="container">
+                <h1>Product List - CRUD - React.js Redux</h1>
+            </div>
+            <a href="/product/new"
+               className="btn btn-danger nuevo-post d-block d-md-inline-block">Add Product &#43;</a>
+        </nav>
+     );
+}
+ 
+export default Header;
