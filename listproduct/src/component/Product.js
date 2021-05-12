0d02f6830a6eaad07684e6937d7839cf50a76797
Santiago Lopez <lopezsantiago35699@gmail.com>
2021-05-12 12:11:10 -0300

    List Product table defined

--- listproduct/src/component/Product.js
+++ listproduct/src/component/Product.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Product = () => {
     return ( 
-        <h1>Desde product</h1>
+        <Fragment>
+            <h2 className="text-center my-5">Product List</h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+            </table>
+        </Fragment>
      );
 }
  
