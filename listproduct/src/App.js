1c47110076b08a666caeb7c4bd4a56300e44248f
Santiago Lopez <lopezsantiago35699@gmail.com>
2021-05-12 13:33:51 -0300

    Redux setting

--- listproduct/src/App.js
+++ listproduct/src/App.js
@@ -7,18 +7,24 @@ import EditProduct from './component/EditProduct'
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+//Redux
+import {Provider} from 'react-redux';
+import store from './store'
+
 function App() {
   return (
     <Router>
-      <Header/>
+      <Provider store={store}>
+        <Header/>
 
-      <div className="container mt-5">
-         <Switch>
-           <Route exact path="/" component={Product}/>
-           <Route exact path="/product/new-product" component={NewProduct}/>
-           <Route exact path="/product/edit-product/:id" component={EditProduct}/>
-         </Switch>
-      </div>
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Product}/>
+            <Route exact path="/product/new-product" component={NewProduct}/>
+            <Route exact path="/product/edit-product/:id" component={EditProduct}/>
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
---
+++ listproduct/src/reducers/ProductReducer.js
@@ -0,0 +1,13 @@
+//initial state to Product 
+const initialState = {
+    product: [],
+    error: null,
+    loading: false
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
