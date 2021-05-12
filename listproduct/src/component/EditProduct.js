284bb1810d6b1a9b6a2a1a3f90c5da43394b3230
Santiago Lopez <lopezsantiago35699@gmail.com>
2021-05-12 12:14:22 -0300

    Edit Product form defined and implemented

--- listproduct/src/component/EditProduct.js
+++ listproduct/src/component/EditProduct.js
@@ -2,7 +2,45 @@ import React from 'react';
 
 const EditProduct = () => {
     return ( 
-        <h1>EditProduct</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-blod">
+                            Edit Product
+                        </h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Product Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="nameP"
+                                    placeholder="Product Name"
+                                />
+                            </div>
+
+                            
+                            <div className="form-group">
+                                <label>Product Price</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    name="priceP"
+                                    placeholder="Product Price"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Save Change</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
